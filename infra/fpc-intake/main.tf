5856ddce58a3036e43881a7c4bdc016657232b98
Elmoge Abukar <e5leadsusa@gmail.com>
2026-05-25 16:20:21 -0700

    First Commit

---
+++ infra/fpc-intake/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_lambda_function" "intake_engine" {
+  filename      = var.lambda_zip_file
+  function_name = "IntakeEngine"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "intake_engine.handler"
+
+  runtime = "python3.9"
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE_NAME = aws_dynamodb_table.intake_table.name
+    }
+  }
+
+  layers = [aws_lambda_layer_version.python_dependencies.arn]
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.iam_for_lambda.name
+}
+
+resource "aws_dynamodb_table" "intake_table" {
+  name         = "IntakeTable"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  key_schema {
+    attribute_name = "id"
+    key_type       = "HASH"
+  }
+}
---
+++ infra/fpc-payment/variables.tf
@@ -0,0 +1,4 @@
+variable "lambda_zip_file" {
+  description = "Path to the Lambda ZIP file for the payment function (e.g. ../lambda-functions/payment_engine.zip)."
+  type        = string
+}
